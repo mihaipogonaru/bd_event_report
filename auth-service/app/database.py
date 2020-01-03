f43e1d19671d21456806fadcdbdb6a06390bbd98
Mihai POGONARU <pogonarumihai@gmail.com>
2020-01-03 22:52:03 +0200

    Use new connection to mysql everytime

--- auth-service/app/database.py
+++ auth-service/app/database.py
@@ -1,19 +1,44 @@
 import MySQLdb
+from functools import wraps
 
 from app.config import DBConfig
 
 
+def use_new_connection(static_func):
+    @wraps(static_func)
+    def decorated_view(*args, **kwargs):
+        db, cursor = Database.new_connection()
+        kwargs['db'] = db
+        kwargs['cursor'] = cursor
+
+        ret = static_func(*args, **kwargs)
+        Database.close_connection(db, cursor)
+
+        return ret
+    return decorated_view
+
+
 class Database:
-    db = MySQLdb.connect(host=DBConfig.db_server, port=DBConfig.db_port,
-                         user=DBConfig.db_user, passwd=DBConfig.db_password, db=DBConfig.db_name)
-    cursor = db.cursor(MySQLdb.cursors.DictCursor)
     err = 'err'
 
     @staticmethod
-    def set_session_read_commited():
-        Database.cursor.execute('SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;')
-        Database.cursor.nextset()
-        Database.db.commit()
+    def new_connection():
+        db = MySQLdb.connect(host=DBConfig.db_server, port=DBConfig.db_port,
+                             user=DBConfig.db_user, passwd=DBConfig.db_password, db=DBConfig.db_name)
+        cursor = db.cursor(MySQLdb.cursors.DictCursor)
+        Database.set_session_read_commited(db, cursor)
+
+        return db, cursor
+
+    @staticmethod
+    def close_connection(db, cursor):
+        cursor.close()
+        db.close()
+
+    @staticmethod
+    def set_session_read_commited(db, cursor):
+        cursor.execute('SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;')
+        db.commit()
 
     @staticmethod
     def call_no_throw(method, *args, **kwargs):
@@ -23,30 +48,30 @@ class Database:
             return Database.err
 
     @staticmethod
-    def insert_user(email: str, password):
-        Database.cursor.callproc('insert_user', (email, password))
-        Database.cursor.nextset()
-        Database.db.commit()
+    @use_new_connection
+    # db and cursor will be injected by use_new_connection, hopefully
+    def insert_user(email: str, password, db=None, cursor=None):
+        cursor.callproc('insert_user', (email, password))
+        db.commit()
 
     @staticmethod
-    def select_user(email: str):
-        Database.cursor.callproc('select_user', (email,))
-
-        result = Database.cursor.fetchall()
-        Database.cursor.nextset()
+    @use_new_connection
+    def select_user(email: str, db=None, cursor=None):
+        cursor.callproc('select_user', (email,))
+        result = cursor.fetchall()
 
         return result[0] if result else None
 
     @staticmethod
-    def delete_user(email: str):
-        Database.cursor.callproc('delete_user', (email,))
-        Database.cursor.nextset()
-        Database.db.commit()
+    @use_new_connection
+    def delete_user(email: str, db=None, cursor=None):
+        cursor.callproc('delete_user', (email,))
+        db.commit()
 
     @staticmethod
-    def get_users():
-        Database.cursor.callproc('get_users')
+    @use_new_connection
+    def get_users(db=None, cursor=None):
+        cursor.callproc('get_users')
+        result = cursor.fetchall()
 
-        result = Database.cursor.fetchall()
-        Database.cursor.nextset()
         return result
