e4c4aee6d586a3ab9d13c875a6c33206f08c88da
Alexandru Stroie <andistroie@yahoo.com>
2020-01-12 21:13:43 +0200

    Docker settings for Andi's laptop

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -3,17 +3,17 @@
 set -x
 set -e
 
-docker build --tag mpogonaru/bd_event-reporter_auth-db:latest auth-db/
+docker build --no-cache --tag mpogonaru/bd_event-reporter_auth-db:andi auth-db/
 #docker push mpogonaru/bd_event-reporter_auth-db:latest
 
-docker build --tag mpogonaru/bd_event-reporter_auth-service:latest auth-service/
+docker build --no-cache --tag mpogonaru/bd_event-reporter_auth-service:andi auth-service/
 #docker push mpogonaru/bd_event-reporter_auth-service:latest
 
-docker build --tag mpogonaru/bd_event-reporter_mongo:latest mongo-db/
+docker build --no-cache --tag mpogonaru/bd_event-reporter_mongo:andi mongo-db/
 #docker push mpogonaru/bd_event-reporter_mongo:latest
 
-docker build --tag mpogonaru/bd_event-reporter_app:latest event-reporter/
+docker build --no-cache --tag mpogonaru/bd_event-reporter_app:andi event-reporter/
 #docker push mpogonaru/bd_event-reporter_app:latest
 
-docker build --tag mpogonaru/bd_event-reporter_nginx:latest nginx/
+docker build --no-cache --tag mpogonaru/bd_event-reporter_nginx:andi nginx/
 #docker push mpogonaru/bd_event-reporter_nginx:latest
