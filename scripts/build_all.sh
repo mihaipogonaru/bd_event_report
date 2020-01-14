44b007bb2c1e89a20c534d917cca3a4a06674a03
Schyther <andrei_daniel.safta@stud.acs.upb.ro>
2020-01-14 04:17:41 +0200

    Modified some of the templates

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -3,17 +3,17 @@
 set -x
 set -e
 
-docker build --no-cache --tag mpogonaru/bd_event-reporter_auth-db:sft auth-db/
+docker build  --tag mpogonaru/bd_event-reporter_auth-db:sft auth-db/
 #docker push mpogonaru/bd_event-reporter_auth-db:latest
 
-docker build --no-cache --tag mpogonaru/bd_event-reporter_auth-service:sft auth-service/
+docker build  --tag mpogonaru/bd_event-reporter_auth-service:sft auth-service/
 #docker push mpogonaru/bd_event-reporter_auth-service:latest
 
-docker build --no-cache --tag mpogonaru/bd_event-reporter_mongo:sft mongo-db/
+docker build  --tag mpogonaru/bd_event-reporter_mongo:sft mongo-db/
 #docker push mpogonaru/bd_event-reporter_mongo:latest
 
-docker build --no-cache --tag mpogonaru/bd_event-reporter_app:sft event-reporter/
+docker build  --tag mpogonaru/bd_event-reporter_app:sft event-reporter/
 #docker push mpogonaru/bd_event-reporter_app:latest
 
-docker build --no-cache --tag mpogonaru/bd_event-reporter_nginx:sft nginx/
+docker build  --tag mpogonaru/bd_event-reporter_nginx:sft nginx/
 #docker push mpogonaru/bd_event-reporter_nginx:latest
