2c1a84702bd8db522c583d2e15e08bfcde02138e
Alexandru Stroie <andistroie@yahoo.com>
2020-01-16 19:56:38 +0200

    Added Android app first version

---
+++ AndroidAPP/EventReporter/app/src/main/java/com/example/eventreporter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.eventreporter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://192.168.29.101");
+    }
+}
